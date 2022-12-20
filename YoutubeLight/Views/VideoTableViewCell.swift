7191110d455fe2ae010fcf5fa141d94cf07e04c5
Kirill Karpovich <kirillkarpovich@Kirills-Air.modem.local>
2022-12-20 10:33:51 +0300

    added custom tableview cell and cache

---
+++ YoutubeLight/Views/VideoTableViewCell.swift
@@ -0,0 +1,91 @@
+//
+//  VideoTableViewCell.swift
+//  YoutubeLight
+//
+//  Created by Kirill Karpovich on 19.12.22.
+//
+
+import UIKit
+
+class VideoTableViewCell: UITableViewCell {
+    
+    @IBOutlet var thumbnailImageView: UIImageView!
+    
+    @IBOutlet var titleLabel: UILabel!
+    
+    @IBOutlet var dateLabel: UILabel!
+    
+    var video:Video?
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    func setCell(_ v:Video) {
+        
+        self.video = v
+        
+        guard self.video != nil else { return }
+        
+        self.titleLabel.text = video?.title
+        
+        let df = DateFormatter()
+        df.dateFormat = "EEEE, MMM d, yyyy"
+        
+        self.dateLabel.text = df.string(from: video!.publishedAt)
+        
+        guard self.video?.thumbnail != "" else { return }
+        
+        
+        // Check cache before downloading data
+        if let cachedData = CacheManager.getVideoCache(self.video!.thumbnail){
+            
+            // Set the thumbnail imageview
+            self.thumbnailImageView.image = UIImage(data: cachedData)
+            return
+        }
+        
+        // Download the thumbnail data
+        let url = URL(string: self.video!.thumbnail)
+        
+        //Get the shared URL session object
+        let session = URLSession.shared
+        
+        // Create a data task
+        let dataTask = session.dataTask(with: url!) { data, response, error in
+            
+            if error == nil && data != nil {
+                
+                // Save the data in the cache
+                CacheManager.setVideoCache(url!.absoluteString, data)
+                
+                // Check that the downloaded url matches the video thumbnail url that this cell is currently set to display
+                if url!.absoluteString != self.video!.thumbnail {
+                    // Video cell has been recycled for another video and no longer matches the thumbnail that was downloaded
+                    return
+                }
+                
+               
+                DispatchQueue.main.async {
+                    // Create the image object
+                    let image = UIImage(data: data!)
+                    
+                    // Set the imageview
+                    self.thumbnailImageView.image = image
+                }
+            }
+        }
+        
+        // Start data task
+        dataTask.resume()
+    }
+
+}
